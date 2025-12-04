30da7038112c0fcb414750436543d4c77e71ef87
G-Mujtaba247 <mujtabaofficial247@gmail.com>
2025-12-04 11:17:25 -1200

    removed unneccesary code

--- user/app/components/SectionHeading.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const SectionHeading = ({title}) => {
-  return (
-    <h2 style={{margin: "32px 0px 32px 0px"}} align="center">{title}</h2>
-  )
-}
-
-export default SectionHeading
